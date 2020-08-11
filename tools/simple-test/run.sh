505f224646e45eee3bde181e0b9c2b39ff84d376
Štěpán Tomsa <stomsa@redhat.com>
2020-08-11 11:37:39 +0200

    Fix duplicated path slash

--- tools/simple-test/run.sh
+++ tools/simple-test/run.sh
@@ -3,4 +3,4 @@ set -e
 
 cd ./tools/simple-test/
 source venv/bin/activate
-python tester.py ../../schemas/system_profile/v1.yaml ../..//scratch/sample.json
+python tester.py ../../schemas/system_profile/v1.yaml ../../scratch/sample.json
