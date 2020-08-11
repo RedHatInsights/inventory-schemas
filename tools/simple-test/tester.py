539dd985a4b9bcf510272d4b5398d6712e4ac6ca
Štěpán Tomsa <stomsa@redhat.com>
2020-08-11 12:14:57 +0200

    Make the validation work
    The specification file contains only definitions. To use it for an
    actual document validation it is necessary to actually use the
    SystemProfile definition. Added that to the in-memory schema.

--- tools/simple-test/tester.py
+++ tools/simple-test/tester.py
@@ -4,11 +4,12 @@ from json import load as json_load
 from jsonschema import validate as jsonschema_validate
 from yaml import safe_load as yaml_safe_load
 
-schema_path = argv[1]
+spec_path = argv[1]
 sample_path = argv[2]
 
 with open(sample_path, 'r') as sample_data:
-    with open(schema_path, 'r') as schema_data:
-        schema_dict = yaml_safe_load(schema_data)
+    with open(spec_path, 'r') as spec_data:
         sample_dict = json_load(sample_data)
+        spec_dict = yaml_safe_load(spec_data)
+        schema_dict = {**spec_dict, "$ref": "#/$defs/SystemProfile"}
         jsonschema_validate(instance=sample_dict, schema=schema_dict)
