8a1331e9a1aa197f67b2cca7e9fbab5350d994bd
Štěpán Tomsa <stomsa@redhat.com>
2020-08-11 12:21:20 +0200

    Refactor context managers
    - Contracted with contexts to save indentation.
    - Kept only loading in the open context.

--- tools/simple-test/tester.py
+++ tools/simple-test/tester.py
@@ -7,9 +7,9 @@ from yaml import safe_load as yaml_safe_load
 spec_path = argv[1]
 sample_path = argv[2]
 
-with open(sample_path, 'r') as sample_data:
-    with open(spec_path, 'r') as spec_data:
-        sample_dict = json_load(sample_data)
-        spec_dict = yaml_safe_load(spec_data)
-        schema_dict = {**spec_dict, "$ref": "#/$defs/SystemProfile"}
-        jsonschema_validate(instance=sample_dict, schema=schema_dict)
+with open(sample_path) as sample_data, open(spec_path) as spec_data:
+    sample_dict = json_load(sample_data)
+    spec_dict = yaml_safe_load(spec_data)
+
+schema_dict = {**spec_dict, "$ref": "#/$defs/SystemProfile"}
+jsonschema_validate(instance=sample_dict, schema=schema_dict)
