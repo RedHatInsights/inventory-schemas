1be46879e13cd91e1fe2b7d50251798f28371207
Štěpán Tomsa <stomsa@redhat.com>
2020-08-12 13:46:27 +0200

    Validate against Draft4
    The Host Inventory uses JSONSchema Draft 4 for validation. It is the
    default of the Connexion framework used by the app. Use the same
    validator here to ensure compatibility.

--- tools/simple-test/tester.py
+++ tools/simple-test/tester.py
@@ -2,6 +2,7 @@ from sys import argv
 
 from json import load as json_load
 from jsonschema import validate as jsonschema_validate
+from jsonschema.validators import Draft4Validator
 from yaml import safe_load as yaml_safe_load
 
 spec_path = argv[1]
@@ -12,4 +13,4 @@ with open(sample_path) as sample_data, open(spec_path) as spec_data:
     spec_dict = yaml_safe_load(spec_data)
 
 schema_dict = {**spec_dict, "$ref": "#/$defs/SystemProfile"}
-jsonschema_validate(instance=sample_dict, schema=schema_dict)
+jsonschema_validate(instance=sample_dict, schema=schema_dict, cls=Draft4Validator)
