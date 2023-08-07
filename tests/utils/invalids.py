0f69956fbdb6cbca77f35b09be44432821e20f75
Lukáš Zapletal <lzap+git@redhat.com>
2023-08-07 21:05:14 +0200

    Add cloud external DNS (#113)
    Signed-off-by: Lukas Zapletal <lzap+git@redhat.com>

--- tests/utils/invalids.py
+++ tests/utils/invalids.py
@@ -27,6 +27,7 @@ INVALID_SYSTEM_PROFILES = (
     {"subscription_auto_attach": ["x" * 101]},
     {"cloud_provider": ["x" * 101]},
     {"public_ipv4_addresses": ["x" * 16]},
+    {"public_dns": ["x" * 101]},
     {"yum_repos": [{"id": "x" * 257}]},
     {"yum_repos": [{"name": "x" * 1025}]},
     {"yum_repos": [{"base_url": "x" * 2049}]},
